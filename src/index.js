88544197a2926542e232511574596a3311c6b9f9
Nikita Melnikov <nik.y.melnikov@gmail.com>
2020-11-11 23:06:30 +0000

    Added express router for auth

--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ const cookieParser = require('cookie-parser');
 const app = express();
 app.use(cookieParser());
 
+const authRouter = require('./authRouter/authRouter');
+
+app.use('/auth', authRouter);
+
 app.listen({
-  port: process.env.PORT,
+  port: process.env.PORT || 8000,
 }, () => {
   console.clear();
   console.log('Started Authentication Service...');
