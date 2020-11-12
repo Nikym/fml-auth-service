aa2d0dff117662541e85e6e76427c3a8af5a589a
Nikita Melnikov <nik.y.melnikov@gmail.com>
2020-11-12 19:16:54 +0000

    Added some comments regarding src/db/index.js

--- src/db/index.js
+++ src/db/index.js
@@ -3,6 +3,15 @@ require('dotenv').config();
 
 const pool = new Pool();
 
+/**
+ * Simplified calls to Postgres by abstracting away from direct
+ * querying.
+ *
+ * getUserByUsername returns a single JSON object describing given
+ * username, if exist.
+ *
+ * createUser adds user to a database.
+ */
 module.exports = {
   query: (text, params) => pool.query(text, params),
   getUserByUsername: async (username) => (await pool.query('SELECT * FROM users WHERE username = $1', [username])).rows[0],
