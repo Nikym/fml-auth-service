5882a627aa9e945c5cb74a46e02a1d4d6b75872b
Nikita Melnikov <nik.y.melnikov@gmail.com>
2020-11-15 16:35:41 +0000

    Written tests for /auth/token

---
+++ src/routes/__tests__/token.test.js
@@ -0,0 +1,32 @@
+const request = require('supertest');
+const app = require('../../app');
+require('dotenv').config();
+
+jest.mock('../../db');
+
+describe('auth/token', () => {
+  it('returns 200 if access token succcessfuly refreshed', async () => {
+    const response = await request(app)
+      .get('/auth/token')
+      .set('Cookie', ['refreshToken=eyJhbGciOiJIUzI1NiJ9.eyJpZCI6IjEyMzQiLCJ1c2VybmFtZSI6ImV4aXN0cyJ9.ioj2N0kWwS3lB9vr_magnS4fYZL3DPBTerFAPq6RblI; Path=/; HttpOnly']);
+
+    expect(response.statusCode).toBe(200);
+    expect(Object.keys(response.body)).toContain('token');
+  });
+
+  it('returns 401 if no refresh token present', async () => {
+    const response = await request(app)
+      .get('/auth/token');
+
+    expect(response.statusCode).toBe(401);
+  });
+
+  it('returns 401 if refresh token does not match stored, and remove refresh token cookie', async () => {
+    const response = await request(app)
+      .get('/auth/token')
+      .set('Cookie', ['refreshToken=eyJhbGciOiJIUzI1NiJ9.eyJpZCI6IjEyMzQiLCJ1c2VybmFtZSI6ImV4aXN0In0.J9aTTx_9KCI9evovXmNN2p4YsUVDo1ahRdCKStcnf54; Path=/; HttpOnly']);
+
+    expect(response.statusCode).toBe(401);
+    expect(response.headers['set-cookie'][0]).toContain('refreshToken=;');
+  });
+});
