25d7664df4f30e3774ef69084458ff5dd118d33e
Nikita Melnikov <nik.y.melnikov@gmail.com>
2020-11-17 15:20:53 +0000

    Added cors module to allow cross site access

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(bodyParser.json());
 
 const authRouter = require('./routes/auth');
 
+app.use(cors());
 app.use('/auth', authRouter);
 
 module.exports = app;
